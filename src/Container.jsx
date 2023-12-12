9a8c08100c2e94f621e49d5453accee3c51f4c7b
zh92 <zhehao92@gmail.com>
2023-12-12 23:01:00 +0800

    added components structure

---
+++ src/Container.jsx
@@ -0,0 +1,9 @@
+import Stats from "./Stats";
+import Textarea from "./Textarea";
+
+export default function Container() {
+    return <main>
+        <Textarea />
+        <Stats />
+    </main>;
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+    return <footer>
+        <small>&copy; Copyright by user. All rights reserved.</small>
+        <small>Last checked limits: 17 days ago</small>
+    </footer>;
+}
